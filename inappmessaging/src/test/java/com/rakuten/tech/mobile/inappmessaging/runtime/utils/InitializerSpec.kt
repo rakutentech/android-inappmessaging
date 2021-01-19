d5a22d4353ec9752d6871556731ef9b969a8317b
Vincent Del Mundo <60253966+vincentdelmundo@users.noreply.github.com>
2021-01-19 15:58:59 +0900

    feat: [SDKCF-3164] refactor uuid generator

--- inappmessaging/src/test/java/com/rakuten/tech/mobile/inappmessaging/runtime/utils/InitializerSpec.kt
+++ inappmessaging/src/test/java/com/rakuten/tech/mobile/inappmessaging/runtime/utils/InitializerSpec.kt
@@ -1,21 +1,23 @@
 package com.rakuten.tech.mobile.inappmessaging.runtime.utils
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.os.Build
 import android.provider.Settings
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
 import androidx.test.core.app.ApplicationProvider
 import androidx.work.Data
 import androidx.work.WorkerParameters
 import androidx.work.testing.WorkManagerTestInitHelper
+import com.nhaarman.mockitokotlin2.eq
+import com.nhaarman.mockitokotlin2.never
 import com.rakuten.tech.mobile.inappmessaging.runtime.BaseTest
 import com.rakuten.tech.mobile.inappmessaging.runtime.InAppMessaging
 import com.rakuten.tech.mobile.inappmessaging.runtime.TestUserInfoProvider
 import com.rakuten.tech.mobile.inappmessaging.runtime.data.repositories.HostAppInfoRepository
 import com.rakuten.tech.mobile.inappmessaging.runtime.exception.InAppMessagingInitializationException
-import org.amshove.kluent.When
-import org.amshove.kluent.calling
-import org.amshove.kluent.itReturns
-import org.amshove.kluent.shouldBeEqualTo
+import org.amshove.kluent.*
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -30,21 +32,26 @@ import java.security.KeyStoreException
 @RunWith(RobolectricTestRunner::class)
 @Config(sdk = [Build.VERSION_CODES.O_MR1])
 class InitializerSpec : BaseTest() {
-    private var workerParameters = Mockito.mock(WorkerParameters::class.java)
-    private var context: Context? = null
+    private val workerParameters = Mockito.mock(WorkerParameters::class.java)
+    private val context = ApplicationProvider.getApplicationContext<Context>()
+    private val mockUtil = Mockito.mock(SharePreferencesUtil::class.java)
+    private val mockMaster = Mockito.mock(MasterKey::class.java)
+    private val mockPref = Mockito.mock(EncryptedSharedPreferences::class.java)
 
     @Before
     fun setup() {
         When calling workerParameters.inputData itReturns Data.EMPTY
-        context = ApplicationProvider.getApplicationContext()
-        Settings.Secure.putString(context?.contentResolver, Settings.Secure.ANDROID_ID, "testid")
+        Settings.Secure.putString(context.contentResolver, Settings.Secure.ANDROID_ID, "testid")
+
+        When calling mockUtil.generateKey(context) itReturns mockMaster
+        When calling mockUtil.createSharedPreference(context, mockMaster) itReturns mockPref
     }
 
     @Test
     fun `should add host app info with basic attributes`() {
         WorkManagerTestInitHelper.initializeTestWorkManager(context!!)
         InAppMessaging.init(
-                context!!,
+                context,
                 "test_sub_key",
                 "",
                 isDebugLogging = false,
@@ -58,10 +65,8 @@ class InitializerSpec : BaseTest() {
 
     @Test
     fun `should not throw exception`() {
-        WorkManagerTestInitHelper.initializeTestWorkManager(ApplicationProvider.getApplicationContext())
-        Initializer.initializeSdk(
-                ApplicationProvider.getApplicationContext(),
-                "test", "", true)
+        WorkManagerTestInitHelper.initializeTestWorkManager(context)
+        Initializer.initializeSdk(context, "test", "", true)
     }
 
     @Test(expected = InAppMessagingInitializationException::class)
@@ -81,17 +86,79 @@ class InitializerSpec : BaseTest() {
     }
 
     @Test(expected = KeyStoreException::class)
-    fun `should throw keystore exception with null android ID`() {
-        val appCtx = ApplicationProvider.getApplicationContext<Context>()
-        val context = Mockito.mock(Context::class.java)
-        Settings.Secure.putString(appCtx.contentResolver, Settings.Secure.ANDROID_ID, null)
-        When calling context.packageName itReturns appCtx.packageName
-        When calling context.contentResolver itReturns appCtx.contentResolver
-        When calling context.packageManager itReturns appCtx.packageManager
-        When calling context.resources itReturns appCtx.resources
+    fun `should throw keystore exception with null android ID and device is android m or above`() {
+        Settings.Secure.putString(context.contentResolver, Settings.Secure.ANDROID_ID, null)
 
         // AndroidKeyStore is not supported by robolectric
-        Initializer.initializeSdk(
-                ApplicationProvider.getApplicationContext(), "test", "", true)
+        Initializer.initializeSdk(context, "test", "", true)
+    }
+
+    @Test
+    @Config(sdk = [Build.VERSION_CODES.LOLLIPOP])
+    fun `should generate uuid when null android ID and device below android m with empty pref`() {
+        val appCtx = ApplicationProvider.getApplicationContext<Context>()
+        Settings.Secure.putString(appCtx.contentResolver, Settings.Secure.ANDROID_ID, null)
+
+        // clear preferences
+        val sharedPref = SharePreferencesUtil.createSharedPreference(context, SharePreferencesUtil.generateKey(context))
+        sharedPref.edit().clear().apply()
+
+        // AndroidKeyStore is not supported by robolectric
+        Initializer.initializeSdk(appCtx, "test", "", true)
+
+        HostAppInfoRepository.instance().getDeviceId().shouldNotBeNullOrEmpty()
+    }
+
+    @Test
+    @Config(sdk = [Build.VERSION_CODES.LOLLIPOP])
+    fun `should generate uuid when null android ID and device below android m with non-empty pref`() {
+        Settings.Secure.putString(context.contentResolver, Settings.Secure.ANDROID_ID, null)
+
+        // add test value
+        val sharedPref = SharePreferencesUtil.createSharedPreference(context, SharePreferencesUtil.generateKey(context))
+        sharedPref.edit().putString(Initializer.ID_KEY, "test_uuid").apply()
+
+        // AndroidKeyStore is not supported by robolectric
+        Initializer.initializeSdk(context, "test", "", true)
+
+        HostAppInfoRepository.instance().getDeviceId() shouldBeEqualTo "test_uuid"
+    }
+
+    @Test
+    fun `should generate uuid using mock with null android ID with empty pref`() {
+        Settings.Secure.putString(context.contentResolver, Settings.Secure.ANDROID_ID, null)
+        val mockEditor = Mockito.mock(SharedPreferences.Editor::class.java)
+
+        When calling mockPref.contains(Initializer.ID_KEY) itReturns false
+        When calling mockPref.edit() itReturns mockEditor
+        When calling mockEditor.putString(any(), any()) itReturns mockEditor
+
+        // AndroidKeyStore is not supported by robolectric
+        Initializer.initializeSdk(context, "test", "", true, mockUtil)
+
+        Mockito.verify(mockUtil).generateKey(context)
+        Mockito.verify(mockUtil).createSharedPreference(context, mockMaster)
+        Mockito.verify(mockPref).contains(Initializer.ID_KEY)
+        Mockito.verify(mockPref).edit()
+        Mockito.verify(mockEditor).putString(eq(Initializer.ID_KEY), any())
+        HostAppInfoRepository.instance().getDeviceId().shouldNotBeNullOrEmpty()
+    }
+
+    @Test
+    fun `should generate uuid using mock with null android ID with non-empty pref`() {
+        Settings.Secure.putString(context.contentResolver, Settings.Secure.ANDROID_ID, null)
+
+        When calling mockPref.contains(Initializer.ID_KEY) itReturns true
+        When calling mockPref.getString(Initializer.ID_KEY, "") itReturns "random_uuid"
+
+        // AndroidKeyStore is not supported by robolectric
+        Initializer.initializeSdk(context, "test", "", true, mockUtil)
+
+        Mockito.verify(mockUtil).generateKey(context)
+        Mockito.verify(mockUtil).createSharedPreference(context, mockMaster)
+        Mockito.verify(mockPref).contains(Initializer.ID_KEY)
+        Mockito.verify(mockPref, never()).edit()
+        Mockito.verify(mockPref).getString(Initializer.ID_KEY, "")
+        HostAppInfoRepository.instance().getDeviceId() shouldBeEqualTo "random_uuid"
     }
 }
