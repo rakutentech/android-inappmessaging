2765ab1a40a3f9562c42ea851a58e671947e3f85
Vincent Del Mundo <60253966+vincentdelmundo@users.noreply.github.com>
2021-04-21 12:32:37 +0900

    fix: enable caching by default and fix app launch only behavior (#78)

--- inappmessaging/src/main/java/com/rakuten/tech/mobile/inappmessaging/runtime/data/repositories/LocalEventRepository.kt
+++ inappmessaging/src/main/java/com/rakuten/tech/mobile/inappmessaging/runtime/data/repositories/LocalEventRepository.kt
@@ -38,6 +38,8 @@ internal interface LocalEventRepository : EventRepository {
         private var instance: LocalEventRepository = LocalEventRepositoryImpl()
         private const val LOCAL_EVENT_KEY = "local_event_list"
 
+        internal var isInitialLaunch = false
+
         fun instance() = instance
     }
 
@@ -108,6 +110,10 @@ internal interface LocalEventRepository : EventRepository {
 
         private fun shouldIgnore(event: Event): Boolean {
             if (event.isPersistentType()) {
+                if (isInitialLaunch) {
+                    isInitialLaunch = false
+                    clearPersistentEvents()
+                }
                 for (currEvent in events) {
                     if (event.getEventType() == currEvent.getEventType() &&
                             event.getEventName() == currEvent.getEventName()) return true
@@ -115,6 +121,14 @@ internal interface LocalEventRepository : EventRepository {
             }
 
             return false
+        }
+
+        private fun clearPersistentEvents() {
+            synchronized(events) {
+                if (events.isNotEmpty()) {
+                    events.removeAll { ev -> ev.isPersistentType() }
+                }
+            }
         }
 
         private fun checkAndResetList(onLaunch: Boolean = false) {
