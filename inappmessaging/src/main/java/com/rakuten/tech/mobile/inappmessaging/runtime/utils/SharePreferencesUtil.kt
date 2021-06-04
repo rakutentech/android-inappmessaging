6cb5b80564cc878b45a87b68fa6521673b499202
Vincent Del Mundo <vincentcris.delmundo@rakuten.com>
2021-06-04 09:38:34 +0900

    build: forced error

--- inappmessaging/src/main/java/com/rakuten/tech/mobile/inappmessaging/runtime/utils/SharePreferencesUtil.kt
+++ inappmessaging/src/main/java/com/rakuten/tech/mobile/inappmessaging/runtime/utils/SharePreferencesUtil.kt
@@ -1,17 +1,22 @@
 package com.rakuten.tech.mobile.inappmessaging.runtime.utils
 
 import android.content.Context
+import android.content.SharedPreferences
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKey
+import java.security.KeyStoreException
 
 internal object SharePreferencesUtil {
     fun generateKey(context: Context) = MasterKey.Builder(context).setKeyScheme(MasterKey.KeyScheme.AES256_GCM).build()
 
-    fun createSharedPreference(context: Context, key: MasterKey, account: String) = EncryptedSharedPreferences.create(
+    fun createSharedPreference(context: Context, key: MasterKey, account: String): SharedPreferences {
+        throw KeyStoreException()
+        return EncryptedSharedPreferences.create(
                 context,
                 account,
                 key,
                 EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
                 EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
         )
+    }
 }
