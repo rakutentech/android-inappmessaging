4c8a1bff1c512b18c00ca8e906549590a28b6f22
leila.gue <leigu.503@gmail.com>
2021-11-30 10:47:52 +0900

    update Picasso memory cache config

--- inappmessaging/src/main/java/com/rakuten/tech/mobile/inappmessaging/runtime/utils/Initializer.kt
+++ inappmessaging/src/main/java/com/rakuten/tech/mobile/inappmessaging/runtime/utils/Initializer.kt
@@ -12,6 +12,7 @@ import com.google.gson.Gson
 import com.rakuten.tech.mobile.inappmessaging.runtime.data.models.HostAppInfo
 import com.rakuten.tech.mobile.inappmessaging.runtime.data.repositories.HostAppInfoRepository
 import com.rakuten.tech.mobile.inappmessaging.runtime.exception.InAppMessagingException
+import com.squareup.picasso.LruCache
 import com.squareup.picasso.Picasso
 import timber.log.Timber
 import java.lang.ClassCastException
@@ -129,10 +130,23 @@ internal object Initializer {
 
             val picasso = Picasso.Builder(context)
                 .downloader(OkHttp3Downloader(client))
+                .memoryCache(LruCache(getMemoryCacheSize()))
                 .build()
             Picasso.setSingletonInstance(picasso)
         } catch (ignored: IllegalStateException) {
             // Picasso instance was already initialized
         }
+    }
+
+    /**
+     * @return 1/8th of the available VM memory in byte.
+     */
+    private fun getMemoryCacheSize(): Int {
+
+        // Get max available VM memory, exceeding this amount will throw an
+        // OutOfMemory exception.
+        val maxMemory = (Runtime.getRuntime().maxMemory()).toInt()
+
+        return maxMemory / 8
     }
 }
