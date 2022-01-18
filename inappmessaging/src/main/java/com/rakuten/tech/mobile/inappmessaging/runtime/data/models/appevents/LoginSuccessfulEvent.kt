1366884bd636461e768059766d163dc588159257
maureenorea.clores <maureenorea.clores@rakuten.com>
2022-01-18 16:15:00 +0900

    Retain bitrise yml

--- inappmessaging/src/main/java/com/rakuten/tech/mobile/inappmessaging/runtime/data/models/appevents/LoginSuccessfulEvent.kt
+++
@@ -1,9 +0,0 @@
-package com.rakuten.tech.mobile.inappmessaging.runtime.data.models.appevents
-
-import com.rakuten.tech.mobile.inappmessaging.runtime.data.enums.EventType
-
-/**
- * Login Successful Event for host app to use.
- * Please note: this will trigger InAppMessaging SDK to update current session data to the new user.
- */
-class LoginSuccessfulEvent : BaseEvent(EventType.LOGIN_SUCCESSFUL, EventType.LOGIN_SUCCESSFUL.name, false)
--- inappmessaging/src/main/java/com/rakuten/tech/mobile/inappmessaging/runtime/exception/InAppMessagingException.kt
+++
@@ -1,9 +0,0 @@
-package com.rakuten.tech.mobile.inappmessaging.runtime.exception
-
-/**
- * Custom exception of In-App Messaging.
- */
-class InAppMessagingException(
-    name: String,
-    cause: Throwable? = null
-) : RuntimeException(name, cause)
