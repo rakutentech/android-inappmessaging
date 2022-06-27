ec42eed95f6faaaae987e429370e5031581c627a
vincentdelmundo <vincentcris.delmundo@rakuten.com>
2022-06-27 11:25:41 +0900

    test: add memory leak

--- test/src/main/java/com/rakuten/test/MainActivity.kt
+++ test/src/main/java/com/rakuten/test/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.rakuten.test
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.rakuten.tech.mobile.inappmessaging.runtime.InAppMessaging
 import com.rakuten.tech.mobile.inappmessaging.runtime.data.models.appevents.AppStartEvent
 
 class MainActivity : AppCompatActivity() {
+    var leakActivity: Activity? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        leakActivity = this
+        SingletonManager.getInstance(this)
         setContentView(R.layout.activity_main)
     }
 
@@ -25,5 +30,19 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         InAppMessaging.instance().unregisterMessageDisplayActivity()
+    }
+}
+
+internal class SingletonManager private constructor(private val context: Context) {
+
+    companion object {
+        private var instance: SingletonManager? = null
+
+        fun getInstance(context: Context): SingletonManager {
+            if (instance == null) {
+                instance = SingletonManager(context);
+            }
+            return instance!!
+        }
     }
 }
