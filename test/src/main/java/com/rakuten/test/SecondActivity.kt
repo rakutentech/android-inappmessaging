fe2bdc14e87da791aa374e9519aa17e8cc45b4e2
vincentdelmundo <vincentcris.delmundo@rakuten.com>
2022-06-29 16:54:50 +0900

    test: force crash in second activity flow

--- test/src/main/java/com/rakuten/test/SecondActivity.kt
+++ test/src/main/java/com/rakuten/test/SecondActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        throw IllegalArgumentException()
         setContentView(R.layout.activity_second)
     }
 }
