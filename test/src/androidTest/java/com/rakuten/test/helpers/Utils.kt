ef76fc234dbdb49a91744710f93dc08cd91d62a1
mau <maureenorea.clores@rakuten.com>
2022-10-20 12:11:42 +0900

    update

--- test/src/androidTest/java/com/rakuten/test/helpers/Utils.kt
+++ test/src/androidTest/java/com/rakuten/test/helpers/Utils.kt
@@ -6,7 +6,8 @@ import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.matcher.ViewMatchers
 import com.rakuten.test.MainActivity
-import com.rakuten.test.R
+import com.rakuten.test.R as AppR
+import com.rakuten.tech.mobile.inappmessaging.runtime.R as IamR
 import okhttp3.mockwebserver.MockWebServer
 
 object Utils {
@@ -18,14 +19,17 @@ object Utils {
 
         // Workaround to trigger `configure` for every test
         // because launchActivity and close does not seem to kill app not triggering ping.
-        onView(ViewMatchers.withId(R.id.reconfigure)).perform(click())
-        onView(ViewMatchers.withId(R.id.edit_config_url)).perform(replaceText(Constants.CONFIG_URL))
+        onView(ViewMatchers.withId(AppR.id.reconfigure)).perform(click())
+        onView(ViewMatchers.withId(AppR.id.edit_config_url)).perform(replaceText(Constants.CONFIG_URL))
         onView(ViewMatchers.withText(android.R.string.ok)).perform(click())
 
-        onView(ViewMatchers.withId(R.id.login_successful)).perform(click())
+        onView(ViewMatchers.withId(AppR.id.login_successful)).perform(click())
+
+        // Provide some time while processing campaigns esp. those with images.
+        // There must be a better way to do this.
         Thread.sleep(sleep)
-        onView(ViewMatchers.withId(com.rakuten.tech.mobile.inappmessaging.runtime.R.id.message_close_button))
-            .perform(click())
+
+        onView(ViewMatchers.withId(IamR.id.message_close_button)).perform(click())
 
         scenario.close()
     }
