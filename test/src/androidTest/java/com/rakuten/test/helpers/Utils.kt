573391aabd514ce6e03a5734fdf9573a252fbf14
mau <maureenorea.clores@rakuten.com>
2022-10-20 10:07:54 +0900

    update

--- test/src/androidTest/java/com/rakuten/test/helpers/Utils.kt
+++ test/src/androidTest/java/com/rakuten/test/helpers/Utils.kt
@@ -1,8 +1,9 @@
 package com.rakuten.test.helpers
 
 import androidx.test.core.app.launchActivity
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.matcher.ViewMatchers
 import com.rakuten.test.MainActivity
 import com.rakuten.test.R
@@ -17,14 +18,14 @@ object Utils {
 
         // Workaround to trigger `configure` for every test
         // because launchActivity and close does not seem to kill app not triggering ping.
-        Espresso.onView(ViewMatchers.withId(R.id.reconfigure)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.edit_config_url)).perform(ViewActions.replaceText(Constants.CONFIG_URL))
-        Espresso.onView(ViewMatchers.withText(android.R.string.ok)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.reconfigure)).perform(click())
+        onView(ViewMatchers.withId(R.id.edit_config_url)).perform(replaceText(Constants.CONFIG_URL))
+        onView(ViewMatchers.withText(android.R.string.ok)).perform(click())
 
-        Espresso.onView(ViewMatchers.withId(R.id.login_successful)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.login_successful)).perform(click())
         Thread.sleep(sleep)
-        Espresso.onView(ViewMatchers.withId(com.rakuten.tech.mobile.inappmessaging.runtime.R.id.message_close_button))
-            .perform(ViewActions.click())
+        onView(ViewMatchers.withId(com.rakuten.tech.mobile.inappmessaging.runtime.R.id.message_close_button))
+            .perform(click())
 
         scenario.close()
     }
