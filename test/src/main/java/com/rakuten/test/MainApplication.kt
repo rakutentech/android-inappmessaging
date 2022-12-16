71796174ad502fdb8a1c1fe353af353b29864cef
mau <maureenorea.clores@rakuten.com>
2022-12-16 16:20:35 +0900

    improve: integrate StricMode tool (SDKCF-6103)

--- test/src/main/java/com/rakuten/test/MainApplication.kt
+++ test/src/main/java/com/rakuten/test/MainApplication.kt
@@ -1,6 +1,9 @@
 package com.rakuten.test
 
 import android.app.Application
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
+import android.os.StrictMode.VmPolicy
 import com.rakuten.tech.mobile.inappmessaging.runtime.InAppMessaging
 
 class MainApplication : Application() {
@@ -9,9 +12,35 @@ class MainApplication : Application() {
     lateinit var settings: IAMSettings
 
     override fun onCreate() {
+        setupStrictMode()
         super.onCreate()
         settings = IAMSettings(this)
         InAppMessaging.configure(this)
         InAppMessaging.instance().registerPreference(provider)
+    }
+
+    /**
+     * StrictMode is most commonly used to catch accidental disk or network access on the application's main thread,
+     * where UI operations are received and animations take place.
+     * https://developer.android.com/reference/android/os/StrictMode
+     */
+    private fun setupStrictMode() {
+        StrictMode.setThreadPolicy(
+            ThreadPolicy.Builder()
+                //.detectDiskReads()
+                //.detectDiskWrites()
+                .detectNetwork() // or .detectAll() for all detectable problems
+                .penaltyLog()
+                .build()
+        )
+        // VM Policy - focused on memory leaks
+        StrictMode.setVmPolicy(
+            VmPolicy.Builder()
+                //.detectLeakedSqlLiteObjects()
+                .detectLeakedClosableObjects()
+                .penaltyLog()
+                .penaltyDeath()
+                .build()
+        )
     }
 }
