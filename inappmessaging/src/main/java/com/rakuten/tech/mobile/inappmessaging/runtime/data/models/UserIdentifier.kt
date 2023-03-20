ca86287aa0bd0eed91cc95fbb4b4468078fd55bd
mau <maureenorea.clores@rakuten.com>
2023-03-20 17:45:15 +0900

    update

--- inappmessaging/src/main/java/com/rakuten/tech/mobile/inappmessaging/runtime/data/models/UserIdentifier.kt
+++ inappmessaging/src/main/java/com/rakuten/tech/mobile/inappmessaging/runtime/data/models/UserIdentifier.kt
@@ -1,13 +1,9 @@
 package com.rakuten.tech.mobile.inappmessaging.runtime.data.models
 
-import com.google.gson.annotations.SerializedName
-
 /**
  * This class represents user identification.
  */
 internal data class UserIdentifier(
-    @SerializedName("id")
     private val id: String,
-    @SerializedName("type")
     private val type: Int,
 )
---
+++ inappmessaging/src/test/java/com/rakuten/tech/mobile/inappmessaging/runtime/data/requests/DisplayPermissionRequestSpec.kt
@@ -0,0 +1,44 @@
+package com.rakuten.tech.mobile.inappmessaging.runtime.data.requests
+
+import com.google.gson.Gson
+import com.rakuten.tech.mobile.inappmessaging.runtime.data.models.UserIdentifier
+import org.amshove.kluent.shouldBeEqualTo
+import org.amshove.kluent.shouldBeEquivalentTo
+import org.junit.Test
+
+@OptIn(ExperimentalStdlibApi::class)
+class DisplayPermissionRequestSpec {
+
+    @Test
+    fun `should serialize DisplayPermissionRequest with correct json field names`() {
+        val testDataClass = DisplayPermissionRequest(
+            campaignId = "test-campaignId",
+            appVersion = "test-appVersion",
+            sdkVersion = "test-sdkVersion",
+            locale = "test-locale",
+            lastPingInMillis = 0L,
+            userIdentifier = listOf(UserIdentifier(id = "test-id", type = 0)),
+        )
+        val json = """
+            >{"campaignId":"test-campaignId","appVersion":"test-appVersion","sdkVersion":"test-sdkVersion",
+            >"locale":"test-locale","lastPingInMillis":0,"userIdentifier":[{"id":"test-id","type":0}],"platform":2}
+        """.trimMargin(">").replace("\n", "")
+        Gson().toJson(testDataClass).shouldBeEqualTo(json)
+    }
+
+    @Test
+    fun `should serialize DisplayPermissionRequest from json field names set`() {
+        val json = """{"lastPingInMillis":0,"userIdentifier":[{"id":"test-id","type":0}],"platform":2}"""
+        val testDataClass = Gson().fromJson(json, DisplayPermissionRequest::class.java)
+        testDataClass.shouldBeEquivalentTo(
+            DisplayPermissionRequest(
+                campaignId = null,
+                appVersion = null,
+                sdkVersion = null,
+                locale = null,
+                lastPingInMillis = 0,
+                userIdentifier = listOf(UserIdentifier(id = "test-id", type = 0)),
+            ),
+        )
+    }
+}
