82ef4b19d0ebd8852ceffa37a566db431938a979
mau <maureenorea.clores@rakuten.com>
2023-03-22 14:38:42 +0900

    comment

--- inappmessaging/src/main/java/com/rakuten/tech/mobile/inappmessaging/runtime/data/responses/DisplayPermissionResponse.kt
+++ inappmessaging/src/main/java/com/rakuten/tech/mobile/inappmessaging/runtime/data/responses/DisplayPermissionResponse.kt
@@ -3,6 +3,9 @@ package com.rakuten.tech.mobile.inappmessaging.runtime.data.responses
 import androidx.annotation.RestrictTo
 import com.google.gson.annotations.SerializedName
 
+/**
+ * This class represents display permission response.
+ */
 @RestrictTo(RestrictTo.Scope.LIBRARY)
 internal data class DisplayPermissionResponse(
     val display: Boolean,
