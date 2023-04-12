0f56ce0aa61f35e5361be88a9fc869457833eee8
mau <maureenorea.clores@rakuten.com>
2023-04-12 17:43:53 +0900

    tests: increase coverage for InAppMessagingConstants

--- inappmessaging/src/main/java/com/rakuten/tech/mobile/inappmessaging/runtime/utils/InAppMessagingConstants.kt
+++ inappmessaging/src/main/java/com/rakuten/tech/mobile/inappmessaging/runtime/utils/InAppMessagingConstants.kt
@@ -3,33 +3,30 @@ package com.rakuten.tech.mobile.inappmessaging.runtime.utils
 /**
  * Constants class for In App Messaging.
  */
-internal class InAppMessagingConstants {
-    companion object {
-        const val ANDROID_PLATFORM_ENUM = 2
+internal object InAppMessagingConstants {
+    const val ANDROID_PLATFORM_ENUM = 2
 
-        // ------------------------------Exception Messages-----------------------------------------------
-        const val EVENT_NAME_EMPTY_EXCEPTION = "Event name can't be empty."
-        const val EVENT_NAME_TOO_LONG_EXCEPTION = "Event name can't exceed 255 characters."
-        const val ARGUMENT_IS_NULL_EXCEPTION = "Argument can't be null."
-        const val ARGUMENT_IS_EMPTY_EXCEPTION = "Argument value is empty."
-        const val VERSION_IS_EMPTY_EXCEPTION = "Version not found in context"
-        const val PACKAGE_NAME_IS_EMPTY_EXCEPTION = "Package Name not found in context"
-        const val LOCALE_IS_EMPTY_EXCEPTION = "Device locale not found in context"
-        const val DEVICE_ID_IS_EMPTY_EXCEPTION = "Device ID not found in context"
-        const val SUBSCRIPTION_KEY_IS_EMPTY_EXCEPTION = "InAppMessaging Subscription Key was not found in context"
+    // ------------------------------Exception Messages-----------------------------------------------
+    const val EVENT_NAME_EMPTY_EXCEPTION = "Event name can't be empty."
+    const val EVENT_NAME_TOO_LONG_EXCEPTION = "Event name can't exceed 255 characters."
+    const val ARGUMENT_IS_NULL_EXCEPTION = "Argument can't be null."
+    const val VERSION_IS_EMPTY_EXCEPTION = "Version not found in context"
+    const val PACKAGE_NAME_IS_EMPTY_EXCEPTION = "Package Name not found in context"
+    const val LOCALE_IS_EMPTY_EXCEPTION = "Device locale not found in context"
+    const val DEVICE_ID_IS_EMPTY_EXCEPTION = "Device ID not found in context"
+    const val SUBSCRIPTION_KEY_IS_EMPTY_EXCEPTION = "InAppMessaging Subscription Key was not found in context"
 
-        // -------------------------------URL Only--------------------------------------------------------
-        const val TEMPLATE_BASE_URL = "http://your.base.url/"
+    // -------------------------------URL Only--------------------------------------------------------
+    const val TEMPLATE_BASE_URL = "http://your.base.url/"
 
-        // ------------------------------ Analytics Event KEYS ---------------------------------------------
-        const val RAT_EVENT_KEY_IMPRESSION = "_rem_iam_impressions"
-        const val RAT_EVENT_KEY_PRIMER = "_rem_iam_pushprimer"
-        const val RAT_EVENT_KEY_EVENT_NAME = "eventName"
-        const val RAT_EVENT_KEY_EVENT_TIMESTAMP = "timestamp"
-        const val RAT_EVENT_KEY_EVENT_CUSTOM_ATTRIBUTE = "customAttributes"
-        const val RAT_EVENT_CAMP_ID = "campaign_id"
-        const val RAT_EVENT_SUBS_ID = "subscription_id"
-        const val RAT_EVENT_IMP = "impressions"
-        const val RAT_EVENT_KEY_PERMISSION = "push_permission"
-    }
+    // ------------------------------ Analytics Event KEYS ---------------------------------------------
+    const val RAT_EVENT_KEY_IMPRESSION = "_rem_iam_impressions"
+    const val RAT_EVENT_KEY_PRIMER = "_rem_iam_pushprimer"
+    const val RAT_EVENT_KEY_EVENT_NAME = "eventName"
+    const val RAT_EVENT_KEY_EVENT_TIMESTAMP = "timestamp"
+    const val RAT_EVENT_KEY_EVENT_CUSTOM_ATTRIBUTE = "customAttributes"
+    const val RAT_EVENT_CAMP_ID = "campaign_id"
+    const val RAT_EVENT_SUBS_ID = "subscription_id"
+    const val RAT_EVENT_IMP = "impressions"
+    const val RAT_EVENT_KEY_PERMISSION = "push_permission"
 }
