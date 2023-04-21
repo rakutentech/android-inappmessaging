48b5ea00d2f8b39c6b7368536d811064558cd3f7
mau <maureenorea.clores@rakuten.com>
2023-04-21 11:29:42 +0900

    tests: increase coverage for inappmessageviewlistener

--- inappmessaging/src/main/java/com/rakuten/tech/mobile/inappmessaging/runtime/view/InAppMessageViewListener.kt
+++ inappmessaging/src/main/java/com/rakuten/tech/mobile/inappmessaging/runtime/view/InAppMessageViewListener.kt
@@ -49,10 +49,8 @@ internal class InAppMessageViewListener(
             when (event.actionMasked) {
                 MotionEvent.ACTION_DOWN -> this.magnifier = Magnifier.Builder(view).build()
                 MotionEvent.ACTION_MOVE -> handleMagnifier(view, event)
-                MotionEvent.ACTION_CANCEL, MotionEvent.ACTION_UP -> if (this.magnifier != null) {
-                    this.magnifier?.dismiss()
-                }
-                else -> if (this.magnifier != null) this.magnifier?.dismiss()
+                MotionEvent.ACTION_CANCEL, MotionEvent.ACTION_UP -> this.magnifier?.dismiss()
+                else -> this.magnifier?.dismiss()
             }
             return view.performClick()
         }
@@ -67,7 +65,7 @@ internal class InAppMessageViewListener(
         if (R.id.opt_out_checkbox == view.id) {
             // If user only checked the opt-out box, just assign the isOptOutChecked variable.
             this.isOptOutChecked = (view as CheckBox).isChecked
-        } else if (R.id.message_close_button == view.id && message != null && !message.isCampaignDismissable) {
+        } else if (R.id.message_close_button == view.id && !message.isCampaignDismissable) {
             // Disable closing the message if not dismissable.
             return
         } else {
@@ -79,7 +77,7 @@ internal class InAppMessageViewListener(
     override fun onKey(v: View?, keyCode: Int, event: KeyEvent?): Boolean {
         return if (event != null && event.action == KeyEvent.ACTION_UP && keyCode == KeyEvent.KEYCODE_BACK) {
             // Disable closing the message if not dismissable.
-            if (message != null && !message.isCampaignDismissable) {
+            if (!message.isCampaignDismissable) {
                 false
             } else {
                 // Handling back button click in coroutine.
@@ -118,13 +116,10 @@ internal class InAppMessageViewListener(
 
     @SuppressLint("NewApi")
     private fun handleMagnifier(view: View, event: MotionEvent) {
-        if (this.magnifier != null) {
+        this.magnifier?.let { magnifier ->
             val viewPosition = IntArray(2)
             view.getLocationOnScreen(viewPosition)
-            this.magnifier?.show(
-                event.rawX - viewPosition[0],
-                event.rawY - viewPosition[1],
-            )
+            magnifier.show(event.rawX - viewPosition[0], event.rawY - viewPosition[1])
         }
     }
 }
