999a6ada5bb2d90c535ddd3910c1842fdd82bba7
mau <maureenorea.clores@rakuten.com>
2023-08-09 14:13:23 +0900

    wip

--- inappmessaging/src/test/java/com/rakuten/tech/mobile/inappmessaging/runtime/InAppMessagingRmcSpec.kt
+++ inappmessaging/src/test/java/com/rakuten/tech/mobile/inappmessaging/runtime/InAppMessagingRmcSpec.kt
@@ -6,9 +6,11 @@ import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.spy
 import com.nhaarman.mockitokotlin2.times
 import com.nhaarman.mockitokotlin2.verify
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.MockedStatic
 import org.mockito.Mockito.*
 import org.robolectric.RobolectricTestRunner
 
@@ -17,68 +19,67 @@ class InAppMessagingRmcSpec {
 
     private lateinit var context: Context
     private lateinit var iamSpy: InAppMessaging.Companion
+    private lateinit var mockRmcHelper: MockedStatic<RmcHelper>
 
     @Before
     fun setup() {
         context = ApplicationProvider.getApplicationContext()
         iamSpy = spy(InAppMessaging)
+        mockRmcHelper = mockStatic(RmcHelper::class.java)
+    }
+
+    @After
+    fun tearDown() {
+        mockRmcHelper.close()
     }
 
     @Test
     fun `should ignore configure if call (with runtime params) is made from app using RMC SDK`() {
-        mockStatic(RmcHelper::class.java).use { mockedRmc ->
-            mockedRmc.`when`<Any> { RmcHelper.isUsingRmc() }.thenReturn(true)
+        mockRmcHelper.`when`<Any> { RmcHelper.isUsingRmc() }.thenReturn(true)
 
-            iamSpy.configure(
-                context,
-                "test-subs-key",
-                "test-config-url",
-            )
+        iamSpy.configure(
+            context,
+            "test-subs-key",
+            "test-config-url",
+        )
 
-            verifyInitializedCalled(0)
-        }
+        verifyInitializedCalled(0)
     }
 
     @Test
     fun `should ignore configure if call (with default params) is made from app using RMC SDK`() {
-        mockStatic(RmcHelper::class.java).use { mockedRmc ->
-            mockedRmc.`when`<Any> { RmcHelper.isUsingRmc() }.thenReturn(true)
+        mockRmcHelper.`when`<Any> { RmcHelper.isUsingRmc() }.thenReturn(true)
 
-            iamSpy.configure(context)
+        iamSpy.configure(context)
 
-            verifyInitializedCalled(0)
-        }
+        verifyInitializedCalled(0)
     }
 
     @Test
     fun `should process configure if call is made from RMC SDK`() {
-        mockStatic(RmcHelper::class.java).use { mockedRmc ->
-            mockedRmc.`when`<Any> { RmcHelper.isUsingRmc() }.thenReturn(true)
+        mockRmcHelper.`when`<Any> { RmcHelper.isUsingRmc() }.thenReturn(true)
 
-            // Simulate call from RMC by adding prefix
-            iamSpy.configure(
-                context,
-                "${RmcHelper.RMC_PREFIX}test-subs-key",
-                "test-config-url",
-            )
+        // Simulate call from RMC by adding prefix
+        iamSpy.configure(
+            context,
+            "${RmcHelper.RMC_PREFIX}test-subs-key",
+            "test-config-url",
+        )
 
-            verifyInitializedCalled(1)
-        }
+        verifyInitializedCalled(1)
     }
 
     @Test
     fun `should process configure if call is made from app not using RMC SDK`() {
-        mockStatic(RmcHelper::class.java).use { mockedRmc ->
-            mockedRmc.`when`<Any> { RmcHelper.isUsingRmc() }.thenReturn(false)
+        mockRmcHelper.`when`<Any> { RmcHelper.isUsingRmc() }.thenReturn(false)
 
-            iamSpy.configure(
-                context,
-                "test-subs-key",
-                "test-config-url",
-            )
+        iamSpy.configure(
+            context,
+            "test-subs-key",
+            "test-config-url",
+        )
 
-            verifyInitializedCalled(1)
-        }
+        verifyInitializedCalled(1)
     }
 
     private fun verifyInitializedCalled(invocations: Int) {
--- inappmessaging/src/test/java/com/rakuten/tech/mobile/inappmessaging/runtime/RmcHelperSpec.kt
+++ inappmessaging/src/test/java/com/rakuten/tech/mobile/inappmessaging/runtime/RmcHelperSpec.kt
@@ -3,26 +3,37 @@ package com.rakuten.tech.mobile.inappmessaging.runtime
 import com.nhaarman.mockitokotlin2.any
 import com.rakuten.tech.mobile.inappmessaging.runtime.utils.CommonUtil
 import org.amshove.kluent.shouldBeEqualTo
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
+import org.mockito.MockedStatic
 import org.mockito.Mockito.mockStatic
 
 class RmcHelperSpec {
 
+    private lateinit var mockCommonUtil: MockedStatic<CommonUtil>
+
+    @Before
+    fun setup() {
+        mockCommonUtil = mockStatic(CommonUtil::class.java)
+    }
+
+    @After
+    fun tearDown() {
+        mockCommonUtil.close()
+    }
+
     @Test
     fun `isUsingRmc should return true`() {
-        mockStatic(CommonUtil::class.java).use { mockedUtil ->
-            mockedUtil.`when`<Any> { CommonUtil.hasClass(any()) }.thenReturn(true)
+        mockCommonUtil.`when`<Any> { CommonUtil.hasClass(any()) }.thenReturn(true)
 
-            RmcHelper.isUsingRmc() shouldBeEqualTo true
-        }
+        RmcHelper.isUsingRmc() shouldBeEqualTo true
     }
 
     @Test
     fun `isUsingRmc should return false`() {
-        mockStatic(CommonUtil::class.java).use { mockedUtil ->
-            mockedUtil.`when`<Any> { CommonUtil.hasClass(any()) }.thenReturn(false)
+        mockCommonUtil.`when`<Any> { CommonUtil.hasClass(any()) }.thenReturn(false)
 
-            RmcHelper.isUsingRmc() shouldBeEqualTo false
-        }
+        RmcHelper.isUsingRmc() shouldBeEqualTo false
     }
 }
