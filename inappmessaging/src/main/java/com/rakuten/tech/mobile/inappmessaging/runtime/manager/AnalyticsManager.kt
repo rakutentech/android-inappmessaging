c97ef8681fb93f7cb7d3c36175f501aa922fa190
mau <maureenorea.clores@rakuten.com>
2024-07-23 15:27:00 +0900

    wip

--- inappmessaging/src/main/java/com/rakuten/tech/mobile/inappmessaging/runtime/manager/AnalyticsManager.kt
+++ inappmessaging/src/main/java/com/rakuten/tech/mobile/inappmessaging/runtime/manager/AnalyticsManager.kt
@@ -1,14 +1,11 @@
 package com.rakuten.tech.mobile.inappmessaging.runtime.manager
 
+import com.rakuten.tech.mobile.inappmessaging.runtime.BuildConfig
 import com.rakuten.tech.mobile.inappmessaging.runtime.EventTrackerHelper
 import com.rakuten.tech.mobile.inappmessaging.runtime.RmcHelper
 import com.rakuten.tech.mobile.inappmessaging.runtime.data.repositories.HostAppInfoRepository
 
-/**
- * @param name1 Standalone event name
- * @param name2 RMC event name
- */
-internal enum class AnalyticsEvent(val name1: String?, val name2: String?) {
+internal enum class AnalyticsEvent(val iamName: String?, val rmcName: String?) {
     IMPRESSION("_rem_iam_impressions", "_rem_rmc_iam_impressions"),
     PUSH_PRIMER(null, "_rem_rmc_iam_pushprimer")
 }
@@ -21,7 +18,7 @@ internal enum class AnalyticsKey(val key: String) {
     DEVICE_ID("device_id"),
     TIMESTAMP("timestamp"),
     CUSTOM_PARAM("cp"),
-    ACCOUNT_ID("acc"),
+    ACCOUNT("acc"),
     APP_ID("aid"),
     IMPRESSIONS("impressions"),
     PUSH_PERMISSION("push_permission")
@@ -30,7 +27,7 @@ internal enum class AnalyticsKey(val key: String) {
 internal object AnalyticsManager {
 
     /**
-     * Sends the event [analyticsEvent]. For RMC, the event is sent to both the host app and SDK accounts.
+     * Sends the event [analyticsEvent]. For RMC, the event is sent to both the host app account and SDK account.
      * This method attaches common metadata to the event such as Device Id etc., so only add custom information that is
      * specific to the event on [data].
      */
@@ -47,24 +44,20 @@ internal object AnalyticsManager {
         params[AnalyticsKey.CUSTOM_PARAM.key] = data
 
         if (RmcHelper.isRmcIntegrated()) {
-            if (analyticsEvent.name2 == null)
-                return
-
-            EventTrackerHelper.sendEvent(analyticsEvent.name2, params)
-
-            val (ratAcc, ratAid) = Pair(888, 2)
-            if (ratAcc == null || ratAid == null)
+            if (analyticsEvent.rmcName == null)
                 return
 
             val paramsCopy = HashMap<String, Any>(params)
-            paramsCopy[AnalyticsKey.ACCOUNT_ID.key] = ratAcc
-            paramsCopy[AnalyticsKey.APP_ID.key] = ratAid
-            EventTrackerHelper.sendEvent(analyticsEvent.name2, paramsCopy)
+            paramsCopy[AnalyticsKey.ACCOUNT.key] = BuildConfig.IAM_RAT_ACC
+            paramsCopy[AnalyticsKey.APP_ID.key] = BuildConfig.IAM_RAT_AID
+
+            EventTrackerHelper.sendEvent(analyticsEvent.rmcName, params)
+            EventTrackerHelper.sendEvent(analyticsEvent.rmcName, paramsCopy)
         } else {
-            if (analyticsEvent.name1 == null)
+            if (analyticsEvent.iamName == null)
                 return
 
-            EventTrackerHelper.sendEvent(analyticsEvent.name1, data)
+            EventTrackerHelper.sendEvent(analyticsEvent.iamName, data)
         }
     }
 }
