ce7a996e0d3195f0ba50ba173c3234bbbf669b5a
mau <maureenorea.clores@rakuten.com>
2024-09-19 14:32:51 +0900

    wip

--- inappmessaging/src/main/java/com/rakuten/tech/mobile/inappmessaging/runtime/extensions/StringExtensions.kt
+++ inappmessaging/src/main/java/com/rakuten/tech/mobile/inappmessaging/runtime/extensions/StringExtensions.kt
@@ -3,13 +3,10 @@ package com.rakuten.tech.mobile.inappmessaging.runtime.extensions
 import android.net.Uri
 import android.util.Patterns
 
-internal fun String?.isValidUrl(): Boolean {
-    return !this.isNullOrEmpty() && (this.isValidWebUrl() || this.isValidDeeplinkUrl())
-}
+internal fun String?.isValidUrl(): Boolean =
+    !this.isNullOrEmpty() && (this.isValidWebUrl() || this.isValidDeeplinkUrl())
 
-private fun String.isValidWebUrl(): Boolean {
-    return Patterns.WEB_URL.matcher(this).matches()
-}
+private fun String.isValidWebUrl(): Boolean = Patterns.WEB_URL.matcher(this).matches()
 
 private fun String.isValidDeeplinkUrl(): Boolean {
     val uri = Uri.parse(this)
--- inappmessaging/src/main/java/com/rakuten/tech/mobile/inappmessaging/runtime/view/InAppMessageBaseView.kt
+++ inappmessaging/src/main/java/com/rakuten/tech/mobile/inappmessaging/runtime/view/InAppMessageBaseView.kt
@@ -171,7 +171,7 @@ internal open class InAppMessageBaseView(context: Context, attrs: AttributeSet?)
             // load the image then display the view
             this.visibility = GONE
             findViewById<ImageView>(R.id.message_image_view)?.let { imgView ->
-                if (!this.imageClickBehavior?.uri.isValidUrl()) {
+                if (this.imageClickBehavior?.uri.isValidUrl()) {
                     imgView.setOnClickListener(this.listener)
                 } // For invalid URL, image content will be non-clickable
                 try {
